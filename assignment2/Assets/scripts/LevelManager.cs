62373e2a1ce3da88ac12a693af78bf6ed21b61e7
Peter Pantelic <a100959@mcast.org.mt>
2017-12-06 11:09:03 +0100

    Menu done

---
+++ assignment2/Assets/scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour {
+
+    public Button startButton;
+    public Button quitButton;
+
+    // Use this for initialization
+    void Start()
+    {
+        startButton = startButton.GetComponent<Button>();
+        quitButton = quitButton.GetComponent<Button>();
+
+    }
+
+    public void StartPressed()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void QuitPressed()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+	
+
+}
